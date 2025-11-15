0f1348c13920ac5859933677e300455fbaf00e4f
Prabhash Ranjan <prabhashranjaner@gmail.com>
2025-11-15 21:10:56 +0530

    background image updated

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import MainSection from "./components/MainSection";
 
 function App() {
   return (
-    <div className="font-primary dark:bg-navy-950 text-navy-900 lg:bg-size[100%] flex min-h-screen flex-col items-center bg-gray-50 bg-[url(/images/bg-desktop-light.jpg)] bg-contain bg-no-repeat pt-[4em] md:pt-24 dark:bg-[url(/images/bg-desktop-dark.jpg)] dark:text-gray-50">
+    <div className="font-primary dark:bg-navy-950 text-navy-900 lg:bg-size[100%] flex min-h-screen flex-col items-center bg-gray-50 bg-[url(/images/bg-desktop-light.jpg)] bg-no-repeat pt-[4em] md:pt-24 lg:bg-contain dark:bg-[url(/images/bg-desktop-dark.jpg)] dark:text-gray-50">
       <div className="flex w-[90vw] max-w-3xl flex-col gap-5">
         <Header />
         <InputForm />
